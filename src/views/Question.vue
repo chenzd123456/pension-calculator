b29258978e0cb3726dc9272cf96034bc7b9a8d59
chenzd123456 <chenzd123456@gmail.com>
2023-03-08 11:49:03 +0800

    pinia 更好的写法

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { storeToRefs } from "pinia";
+
 import inputbox from "@/components/inputbox.vue";
 import myButton from "@/components/my-button.vue";
 
 import { usePensionStore } from "@/stores/pension.js";
-import router from '@/router'
+import router from "@/router";
 
 const pensionStore = usePensionStore();
+
+const { currentAge, retirementAge, monthlyIncome, bankSavings } =
+  storeToRefs(pensionStore);
 
 function calculate() {
   router.replace("/report");
@@ -16,25 +21,25 @@ function calculate() {
     id="current-age"
     label="当前年龄？"
     unit="岁"
-    v-model.number="pensionStore.currentAge"
+    v-model.number="currentAge"
   ></inputbox>
   <inputbox
     id="retirement-age"
     label="退休年龄？"
     unit="岁"
-    v-model.number="pensionStore.retirementAge"
+    v-model.number="retirementAge"
   ></inputbox>
   <inputbox
     id="monthly-income"
     label="月收入是多少？"
     unit="元"
-    v-model.number="pensionStore.monthlyIncome"
+    v-model.number="monthlyIncome"
   ></inputbox>
   <inputbox
     id="bank-savings"
     label="存款多少？"
     unit="元"
-    v-model.number="pensionStore.bankSavings"
+    v-model.number="bankSavings"
   ></inputbox>
   <myButton id="calculate-button" text="计算" @click="calculate"></myButton>
 </template>
